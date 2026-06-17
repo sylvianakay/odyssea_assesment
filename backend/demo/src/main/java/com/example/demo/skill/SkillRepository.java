e77d1005022bc77a04ecbce801a2f588fb0f867a
Anastasia-Sylvana Kakarontza <sylvkakarontza@gmail.com>
2026-06-17 14:14:39 +0300

    Refactor SkillRepository method signature
    - Updated the method to find skills by a set of names, enhancing type specificity and improving performance.

--- backend/demo/src/main/java/com/example/demo/skill/SkillRepository.java
+++ backend/demo/src/main/java/com/example/demo/skill/SkillRepository.java
@@ -2,13 +2,13 @@ package com.example.demo.skill;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface SkillRepository extends JpaRepository<Skill, Long> {
 
 	Optional<Skill> findByName(String name);
 
-	List<Skill> findByNameIn(Collection<String> names);
+	List<Skill> findAllByNameIn(Set<String> names);
 }
